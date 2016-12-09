bf46cb8e0c27e74538afe780f5f6f998c218568b
Lilian Besson <lbesson@ens-cachan.fr>
2016-12-09 19:23:48 +0100

    Added two variants of the UCB-V policy:
    - MOSS from [Audibert & Bubeck, 10]
    - UCB-Tuned from [Auer et al. 02]

---
+++ Policies/MOSS.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+""" The MOSS policy for bounded bandits.
+Reference: [Audibert & Bubeck, 10].
+"""
+
+__author__ = "Lilian Besson"
+__version__ = "0.1"
+
+from math import sqrt, log
+import numpy as np
+
+from .UCBV import UCBV
+
+
+class MOSS(UCBV):
+    """ The MOSS policy for bounded bandits.
+    Reference: [Audibert & Bubeck, 10].
+    """
+
+    def __init__(self, nbArms, amplitude=1., lower=0.):
+        super(MOSS, self).__init__(nbArms, amplitude, lower)
+
+    def __str__(self):
+        return "MOSS"
+
+    def computeIndex(self, arm):
+        if self.pulls[arm] < 2:
+            return float('+inf')
+        else:
+            mean = self.rewards[arm] / self.pulls[arm]   # Mean estimate
+            return mean + sqrt(max(0, log(self.t / (self.nbArms * self.pulls[arm]))) / self.pulls[arm])
---
+++ Policies/UCBTuned.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+""" The UCB-Tuned policy for bounded bandits, with a tuned variance correction term.
+Reference: [Auer et al. 02].
+"""
+
+__author__ = "Olivier Cappé, Aurélien Garivier"
+__version__ = "$Revision: 1.7 $"
+
+from math import sqrt, log
+import numpy as np
+
+from .UCBV import UCBV
+
+
+class UCBTuned(UCBV):
+    """ The UCB-Tuned policy for bounded bandits, with a tuned variance correction term.
+    Reference: [Auer et al. 02].
+    """
+
+    def __init__(self, nbArms, amplitude=1., lower=0.):
+        super(UCBTuned, self).__init__(nbArms, amplitude, lower)
+
+    def __str__(self):
+        return "UCBTuned"
+
+    def computeIndex(self, arm):
+        if self.pulls[arm] < 2:
+            return float('+inf')
+        else:
+            mean = self.rewards[arm] / self.pulls[arm]   # Mean estimate
+            variance = (self.rewardsSquared[arm] / self.pulls[arm]) - mean ** 2  # Variance estimate
+            # Correct variance estimate
+            variance += sqrt(2.0 * log(self.t) / self.pulls[arm])
+            return mean + sqrt(log(self.t) * variance / self.pulls[arm])
