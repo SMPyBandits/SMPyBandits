329f3323092785c3244de4d8d4080c23a8c63195
Lilian Besson <lbesson@ens-cachan.fr>
2016-12-09 16:00:56 +0100

    On this example, K = 2 arms [B(0.1, 0.9)], and M = 2 users, MEGA (with well-tuned parameters) work better than MusicalChair(T0=1%). T=10000, nbRepetitions=20.

--- configuration_multiplayers.py
+++ configuration_multiplayers.py
@@ -102,14 +102,14 @@ configuration = {
     "averageOn": 1e-3,  # Average the final rank on the 1.0% last time steps
     # --- Arms
     "environment": [
-        # {   # A damn simple problem: 2 arms, one bad, one good
-        #     "arm_type": Bernoulli,
-        #     "params": [0.1, 0.9]
-        # },
-        {   # A very very easy problem: 3 arms, one bad, one average, one good
+        {   # A damn simple problem: 2 arms, one bad, one good
             "arm_type": Bernoulli,
-            "params": [0.1, 0.5, 0.9]
+            "params": [0.1, 0.9]
         },
+        # {   # A very very easy problem: 3 arms, one bad, one average, one good
+        #     "arm_type": Bernoulli,
+        #     "params": [0.1, 0.5, 0.9]
+        # },
         # {   # A very easy problem (9 arms), but it is used in a lot of articles
         #     "arm_type": Bernoulli,
         #     "params": [t / 10.0 for t in range(1, 10)]
@@ -194,7 +194,8 @@ if TEST_MULTIPLAYER_POLICY:
         # "players": Selfish(NB_PLAYERS, MusicalChair, nbArms, Time0=0.05, Time1=HORIZON).childs  # OK Estimate nbPlayers in Time0 initial rounds
         # "players": Selfish(NB_PLAYERS, MusicalChair, nbArms, Time0=0.01, Time1=HORIZON).childs  # OK Estimate nbPlayers in Time0 initial rounds
         # --- Using single-player MEGA policy
-        "players": Selfish(NB_PLAYERS, MEGA, nbArms, c=0.1, d=0.01, p0=0.6, alpha=0.5, beta=0.8).childs  # XXX parameters
+        "players": Selfish(NB_PLAYERS, MEGA, nbArms, c=0.1, d=0.5, p0=0.6, alpha=0.5, beta=0.8).childs  # XXX parameters
+        # "players": Selfish(NB_PLAYERS, MEGA, nbArms, c=0.1, d=0.01, p0=0.6, alpha=0.5, beta=0.8).childs  # XXX parameters
     })
 # TODO the EvaluatorMultiPlayers should regenerate the list of players in every repetitions, to have at the end results on the average behavior of these randomized multi-players policies
 
