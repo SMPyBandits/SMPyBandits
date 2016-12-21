abe480713a9b93b2aa0031de708b74179f903a9f
Lilian Besson <lbesson@ens-cachan.fr>
2016-12-21 23:45:19 +0100

    Added the Exp3 algorithm (and 3 variants).
    - Reference: [Regret Analysis of Stochastic and Nonstochastic Multi-armed Bandit Problems, S.Bubeck & N.Cesa-Bianchi, §3.1](http://research.microsoft.com/en-us/um/people/sebubeck/SurveyBCB12.pdf)
    - Exp3Decreasing: Decreasing gamma with the time: sqrt(log(K) / (t * K)).
    - Exp3SoftMix: Decreasing gamma with the time: c * log(t) / t.
    - Exp3WithHorizon: Fixed gamma, small, knowing the horizon: sqrt(2 * log(K) / (horizon * K)).

---
+++ Policies/Exp3.py
@@ -0,0 +1,148 @@
+# -*- coding: utf-8 -*-
+""" The Exp3 index policy.
+Reference: [Regret Analysis of Stochastic and Nonstochastic Multi-armed Bandit Problems, S.Bubeck & N.Cesa-Bianchi, §3.1](http://research.microsoft.com/en-us/um/people/sebubeck/SurveyBCB12.pdf)
+"""
+
+__author__ = "Lilian Besson"
+__version__ = "0.1"
+
+import numpy as np
+from .BasePolicy import BasePolicy
+
+# self.unbiased is a flag to know if the rewards are used as biased estimator,
+# ie just r_t, or unbiased estimators, r_t / p_t
+UNBIASED = True
+UNBIASED = False
+
+# Default gamma parameter
+GAMMA = 0.5
+
+
+class Exp3(BasePolicy):
+    """ The Exp3 index policy.
+    Reference: [Regret Analysis of Stochastic and Nonstochastic Multi-armed Bandit Problems, S.Bubeck & N.Cesa-Bianchi, §3.1](http://research.microsoft.com/en-us/um/people/sebubeck/SurveyBCB12.pdf)
+    """
+
+    def __init__(self, nbArms, gamma=GAMMA,
+                 unbiased=UNBIASED, lower=0., amplitude=1.):
+        super(Exp3, self).__init__(nbArms, lower=lower, amplitude=amplitude)
+        if gamma is None:  # Use a default value for the gamma parameter
+            gamma = np.sqrt(np.log(nbArms) / nbArms)
+        assert 0 < gamma <= 1, "Error: the 'gamma' parameter for Exp3 class has to be in (0, 1]."
+        self._gamma = gamma
+        self.unbiased = unbiased
+        # Internal memory
+        self.weights = np.ones(nbArms) / nbArms
+
+    def startGame(self):
+        super(Exp3, self).startGame()
+        self.weights.fill(1. / self.nbArms)
+
+    def __str__(self):
+        return "Exp3(gamma: {})".format(self.gamma)
+
+    # This decorator @property makes this method an attribute, cf. https://docs.python.org/2/library/functions.html#property
+    @property
+    def gamma(self):
+        return self._gamma
+
+    @property
+    def trusts(self, p_t=None):
+        # Mixture between the weights and the uniform distribution
+        p_t = ((1 - self.gamma) * self.weights) + (self.gamma / self.nbArms)
+        return p_t / np.sum(p_t)
+
+    def getReward(self, arm, reward):
+        super(Exp3, self).__init__(arm, reward)  # XXX Call to BasePolicy
+        # Update weight of THIS arm, with this biased or unbiased reward
+        if self.unbiased:
+            reward /= self.trusts[arm]
+        # Multiplicative weights
+        self.weights[arm] *= np.exp(reward * (self.gamma / self.nbArms))
+        # Renormalize weights at each step
+        self.weights /= np.sum(self.weights)
+
+    # --- Choice methods
+
+    def choice(self):
+        # Force to first visit each arm once in the first steps
+        if self.t < self.nbArms:
+            return self.t % self.nbArms  # TODO? random permutation instead of deterministic order!
+        else:
+            return np.random.choice(self.nbArms, p=self.trusts)
+
+    def choiceWithRank(self, rank=1):
+        if (self.t < self.nbArms) or (rank == 1):
+            return self.choice()
+        else:
+            return np.random.choice(self.nbArms, size=rank, replace=False, p=self.trusts)[rank - 1]
+
+    def choiceFromSubSet(self, availableArms='all'):
+        if (self.t < self.nbArms) or (availableArms == 'all') or (len(availableArms) == self.nbArms):
+            return self.choice()
+        else:
+            return np.random.choice(availableArms, p=self.trusts[availableArms])
+
+    def choiceMultiple(self, nb=1):
+        if (self.t < self.nbArms) or (nb == 1):
+            return self.choice()
+        else:
+            return np.random.choice(self.nbArms, size=nb, replace=False, p=self.trusts)
+
+
+# --- Two special cases
+
+class Exp3Decreasing(Exp3):
+    """ Exp3 with decreasing gamma eta_t."""
+
+    def __str__(self):
+        return "Exp3(decreasing)"
+
+    # This decorator @property makes this method an attribute, cf. https://docs.python.org/2/library/functions.html#property
+    @property
+    def gamma(self):
+        """ Decreasing gamma with the time: sqrt(log(K) / (t * K)).
+
+        - Cf. Theorem 3.1 case #2 of [Bubeck & Cesa-Bianchi, 2012].
+        """
+        return np.sqrt(np.log(self.nbArms) / (self.t * self.nbArms))
+
+
+class Exp3SoftMix(Exp3):
+    """ Another Exp3 with decreasing gamma eta_t."""
+
+    def __str__(self):
+        return "Exp3(decreasing)"
+
+    # This decorator @property makes this method an attribute, cf. https://docs.python.org/2/library/functions.html#property
+    @property
+    def gamma(self, c=None):
+        """ Decreasing gamma with the time: c * log(t) / t.
+
+        - Cf. [Cesa-Bianchi & Fisher, 1998].
+        - Default value for c = sqrt(log(K) / K).
+        """
+        if c is None:
+            c = np.sqrt(np.log(self.nbArms) / self.nbArms)
+        return c * np.log(self.t) / self.t
+
+
+class Exp3WithHorizon(Exp3):
+    """ Exp3 with fixed gamma eta chosen with a knowledge of the horizon."""
+
+    def __init__(self, nbArms, horizon, lower=0., amplitude=1.):
+        super(Exp3WithHorizon, self).__init__(nbArms, lower=lower, amplitude=amplitude)
+        assert horizon > 0, "Error: the 'horizon' parameter for SoftmaxWithHorizon class has to be > 0."
+        self.horizon = horizon
+
+    def __str__(self):
+        return "Exp3(horizon: {})".format(self.horizon)
+
+    # This decorator @property makes this method an attribute, cf. https://docs.python.org/2/library/functions.html#property
+    @property
+    def gamma(self):
+        """ Fixed gamma, small, knowing the horizon: sqrt(2 * log(K) / (horizon * K)).
+
+        - Cf. Theorem 3.1 case #1 of [Bubeck & Cesa-Bianchi, 2012].
+        """
+        return np.sqrt(2 * np.log(self.nbArms) / (self.horizon * self.nbArms))
