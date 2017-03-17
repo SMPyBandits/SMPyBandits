f16a399bb4acffa8f0aae0b7c86df80b46821d3f
Lilian Besson <lbesson@ens-cachan.fr>
2017-03-17 17:59:38 +0100

    Improved this line, thanks to.. well, my answer to http://stackoverflow.com/a/42862472/

--- Environment/CollisionModels.py
+++ Environment/CollisionModels.py
@@ -57,11 +57,12 @@ def onlyUniqUserGetsReward(t, arms, players, choices, rewards, pulls, collisions
     - This is the default collision model, cf. https://arxiv.org/abs/0910.2065v3 collision model 1.
     - The numpy array 'choices' is increased according to the number of users who collided (it is NOT binary).
     """
-    nbCollisions = [np.sum(choices == arm) - 1 for arm in range(len(arms))]
+    # nbCollisions = [np.sum(choices == arm) - 1 for arm in range(len(arms))]  # XXX it is slow!
+    nbCollisions = np.bincount(choices)  # XXX this is faster!
     # print("onlyUniqUserGetsReward() at time t = {}, nbCollisions = {}.".format(t, nbCollisions))  # DEBUG
     # if np.max(nbCollisions) >= 1:  # DEBUG
     #     print("- onlyUniqUserGetsReward: some collisions on channels {} at time t = {} ...".format(np.nonzero(np.array(nbCollisions) >= 1)[0], t))  # DEBUG
-    for i, player in enumerate(players):
+    for i, player in enumerate(players):  # Loop is needed because player is needed
         if nbCollisions[choices[i]] < 1:  # No collision
             rewards[i] = arms[choices[i]].draw(t)
             player.getReward(choices[i], rewards[i])
