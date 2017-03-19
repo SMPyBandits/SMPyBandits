32d1b890cc72551c33341415a7632f64fe6bdb62
Lilian Besson <lbesson@ens-cachan.fr>
2017-03-19 17:32:59 +0100

    Better doc for Policies.Uniform

--- Policies/Uniform.py
+++ Policies/Uniform.py
@@ -14,17 +14,24 @@ class Uniform(BasePolicy):
     """
 
     def __init__(self, nbArms, lower=0., amplitude=1.):
+        """Nothing to do."""
         self.nbArms = nbArms
 
+    def __str__(self):
+        return "U(1..{})".format(self.nbArms)
+
     def startGame(self):
+        """Nothing to do."""
         pass
 
     def getReward(self, arm, reward):
+        """Nothing to do."""
         pass
 
     def choice(self):
+        """Uniform random choice between 0 and nbArms - 1 (included)."""
         return randint(0, self.nbArms - 1)
 
     def choiceWithRank(self, rank=1):
-        """ Ignore the rank."""
+        """Ignore the rank!"""
         return self.choice()
