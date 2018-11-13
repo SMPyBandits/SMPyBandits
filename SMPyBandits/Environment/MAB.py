e1623d36533c8551c6c4e82e35e2b1c1b72a9329
Lilian Besson <lbesson@ens-cachan.fr>
2018-11-13 15:21:26 +0100

    The 'normed' kwarg was deprecated in Matplotlib 2.1 and will be removed in 3.1. Use 'density' instead.

--- SMPyBandits/Environment/MAB.py
+++ SMPyBandits/Environment/MAB.py
@@ -377,7 +377,7 @@ class MAB(object):
         # Now plot
         fig = plt.figure()
         for armId, arm in enumerate(arms):
-            plt.hist(rewards[armId, :], bins=200, normed=True, color=colors[armId], label='$%s$' % repr(arm), alpha=0.7)
+            plt.hist(rewards[armId, :], bins=200, density=True, color=colors[armId], label='$%s$' % repr(arm), alpha=0.7)
         legend()
         plt.xlabel("Rewards")
         plt.ylabel("Mass repartition of the rewards")
