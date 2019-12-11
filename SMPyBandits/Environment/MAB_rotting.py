5ebb34f309436e3ad04459f52dcdfa3ce99b70ce
Julien <julien@MacBook-Air.local>
2019-12-11 11:33:34 +0100

    feat(Experiment): add part of the experiments

---
+++ SMPyBandits/Environment/MAB_rotting.py
@@ -0,0 +1,63 @@
+"""
+author : Julien SEZNEC
+Code to launch (rotting) bandit games.
+It is code in a functional programming way : each execution return arrays related to each run.
+"""
+
+import time
+import numpy as np
+import logging
+from joblib import Parallel, delayed
+
+REPETITIONS = 1000
+HORIZON = 10000
+
+def repetedRuns(policy, arms, rep = REPETITIONS, T = HORIZON, parallel = True, oracle = False):
+    rew = np.empty(shape = (rep, T))
+    noisy_rew = np.empty(shape = (rep, T))
+    time = np.empty(shape = (rep, T))
+    pulls = np.empty(shape=(rep, T))
+    cumul_pulls = np.empty(shape=(rep, len(arms)))
+    if parallel:
+        res = Parallel(n_jobs=parallel)(delayed(singleRun)(policy,arms, T, r, oracle) for r in range(rep))
+    else:
+        res = [singleRun(policy,arms, T=T) for _ in range(rep)]
+    rew[:, :] =  np.array([r['cumul'] for r in res ])
+    noisy_rew[:, :] = np.array([r['noisy_cumul'] for r in res])
+    time[:, :] = np.array([r['time'] for r in res ])
+    pulls[:,:] = np.array([r['pulls'] for r in res ])
+    cumul_pulls[:,:] = np.array([r['cumul_pulls'] for r in res ])
+    return rew, noisy_rew, time, pulls, cumul_pulls
+
+def singleRun(policy, arms, T = HORIZON,rep_index = 0, oracle=False):
+    myArms = [arm[0](**arm[1]) for arm in arms]
+    if oracle:
+        policy[1]['arms'] = myArms
+    myPolicy = policy[0](len(myArms), **policy[1])
+    logging.debug(str(rep_index) + ' ' + myPolicy.__str__())
+    res = play(myArms, myPolicy, T, Oracle=oracle)
+    return {
+      'cumul': np.array(res['rewards']).cumsum(),
+      'noisy_cumul': np.array(res['noisy_rewards']),
+      'time' : np.array(res['time']),
+      'pulls' : np.array(res['pulls']),
+      'cumul_pulls' : np.array(res['cumul_pulls'])
+    }
+
+
+def play(arms, policy, T, Oracle= False):
+    noisy_rewards = []
+    rewards = []
+    times = []
+    pulls = []
+    for t in range(T):
+        start = time.time()
+        choice = policy.choice()
+        reward = arms[choice].mu
+        noisy_reward = arms[choice].draw(t) if not Oracle else arms[choice].oracle_draw(t)
+        policy.getReward(choice, noisy_reward)
+        times.append(time.time() - start)
+        noisy_rewards.append(noisy_reward)
+        rewards.append(reward)
+        pulls.append(choice)
+    return {'rewards': rewards, 'noisy_rewards': noisy_rewards, 'time': times, 'pulls': pulls, 'cumul_pulls' : policy.pulls}
---
+++ SMPyBandits/Experiment/Seznec19a_Fig1/main.py
@@ -0,0 +1,73 @@
+from ...Arms import PullDecayingGaussian, constant, abruptSingleDecay
+from ...Policies import *
+from ...Environment.MAB_rotting import repetedRuns
+import numpy as np
+import datetime
+import os
+import logging
+
+date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S_")
+path = os.path.join('./data', date + "fig1")
+os.mkdir(path)
+logging.basicConfig(filename=os.path.join(path, 'logging.log'), level=logging.INFO, format='%(asctime)s %(message)s')
+
+
+PARALLEL = -1 # Set positive int to indicate the number of core, -1 to use all the cores, and False to not parallelize
+REPETITIONS = 291 # Set the number of repetitions
+HORIZON = 10000 # Horizon T
+sigma = 1 #Gaussian noise std
+
+### SET L/2 in figure 1
+mus = [.01 * 1.25 ** i for i in range(30)]
+logging.info("CONFIG : CPU %s" % os.cpu_count())
+logging.info("CONFIG : REPETITIONS %s" % REPETITIONS)
+logging.info("CONFIG : HORIZON %s" % HORIZON)
+logging.info("CONFIG : SIGMA %s" % sigma)
+
+### SET Policies
+policies = [
+  [FEWA, {'alpha': .03, 'delta': 1}],
+  [FEWA, {'alpha': .06, 'delta': 1}],
+  [FEWA, {'alpha': .1, 'delta': 1}],
+  [EFF_FEWA, {'alpha' : 0.06, 'delta':1, 'm':1.1}],
+  [wSWA, {'alpha' : 0.002}],
+  [wSWA, {'alpha' : 0.02}],
+  [wSWA, {'alpha' : 0.2}],
+]
+
+for i, pol in enumerate(policies):
+  logging.info("POLICY " + str(i) + " : " + str(pol[0](nbArms=2, **pol[1])))
+
+
+for i, policy in enumerate(policies):
+  logging.info("STARTING POLICY " + str(i) + " : " + str(pol[0](nbArms=2, **pol[1])))
+  noisy_reward_res =[]
+  regret_res = []
+  time_res = []
+  overpull_res =[]
+  for m, mu in enumerate(mus):
+    logging.info("EVENT : GAME %s $\mu = %s$" % (m, mu))
+    print(mu)
+    ### SET K arms
+    arms = [
+      [
+        PullDecayingGaussian,
+        {'function': constant, 'sigma': sigma, 'functionName': 'constant', 'functionArgs': {"mu": 0}}
+      ],
+      [
+        PullDecayingGaussian,
+        {'function': abruptSingleDecay, 'sigma': sigma,
+         'functionName': 'AbruptSingleDecay', 'functionArgs': {"mu": mu, "switchPoint": HORIZON / 4}}
+      ],
+    ]
+    rew, noisy_rew, time, pulls, cumul_pulls = repetedRuns(arms, policies, rep=REPETITIONS, T=HORIZON, parallel=PARALLEL)
+    oracle_rew, noisy_oracle_rew, oracle_time, oracle_pull, oracle_cumul_pulls = repetedRuns(arms, [[GreedyOracle, {}]], rep=1, T=HORIZON, oracle=True)
+    regret = oracle_rew - rew
+    logging.info("EVENT : SAVING ... ")
+    regret_res.append(regret)
+    np.save(os.path.join(path, 'regret'), np.array(regret_res))
+logging.info("EVENT : END ... ")
+
+
+
+
