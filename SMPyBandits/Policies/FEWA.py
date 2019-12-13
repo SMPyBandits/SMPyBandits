fix(FEWA) : repare efficient window computation and confidence band tuning

SMPyBandits/Policies/FEWA.py
@@ -42,19 +42,20 @@ class EFF_FEWA(BasePolicy):
         self.nbArms = nbArms
         self.subgaussian = subgaussian
         self.statistics = np.ones(shape=(3, self.nbArms, 2)) * np.nan
-        # [0,:,:] : current statistics, [1,:,:]: pending statistics, [3,:,:]: number of sample in the pending statistics
+        # [0,:,:] : current statistics, [1,:,:]: pending statistics, [2,:,:]: number of sample in the pending statistics
         self.windows = np.array([1, int(np.ceil(m))])
-        self.outlogconst = np.sqrt(self.windows * np.sqrt(8 * self.alpha * self.subgaussian ** 2))
+        self.outlogconst = self._append_thresholds(self.windows)
         self.delta = delta if delta!= None else 1
-        self.plot_delta = delta != None
+        self.inlogconst = 1/delta**(1/alpha) if delta!= None else 1
         self.armSet = np.arange(nbArms)
         self.grid = m
 
     def __str__(self):
-        if self.plot_delta:
-            return r"EFF_FEWA($\alpha={:.3g}, \, m={:.3g}$)".format(self.alpha, self.grid)
+        if self.delta != None:
+            return r"EFF_FEWA($\alpha={:.3g}, \, \delta={:.3g}, \, m={:.3g}$)".format(self.alpha, self.delta, self.grid)
         else:
-            return r"EFF_FEWA($\alpha={:.3g}, \, \delta ={:.3g}, \, m={:.3g}$)".format(self.alpha, self.delta, self.grid)
+            return r"EFF_FEWA($\alpha={:.3g}, \, m={:.3g}$)".format(self.alpha, self.grid)
+
 
     def getReward(self, arm, reward):
         super(EFF_FEWA, self).getReward(arm, reward)
@@ -77,8 +78,7 @@ class EFF_FEWA(BasePolicy):
         remainingArms = self.armSet.copy()
         i = 0
         selected = remainingArms[np.isnan(self.statistics[0, :, i])]
-        delta_inv = self._confidence_level_inv()
-        sqrtlogt = np.sqrt(np.log(delta_inv))
+        sqrtlogt = np.sqrt(np.log(self._inlog()))
         while len(selected) == 0 :
             thresh = np.max(self.statistics[0, remainingArms, i]) - sqrtlogt * self.outlogconst[i]
             remainingArms = remainingArms[self.statistics[0, remainingArms, i] >= thresh]
@@ -88,16 +88,16 @@ class EFF_FEWA(BasePolicy):
         return selected[np.argmin(self.pulls[selected])]
 
     def _append_thresholds(self, w):
-        return np.sqrt(8 * w * self.subgaussian ** 2)
+        return np.sqrt(8 * w * self.alpha * self.subgaussian ** 2)
 
-    def _confidence_level_inv(self):
-        return self.t ** self.alpha / self.delta
+    def _inlog(self):
+        return self.inlogconst * self.t
 
     def startGame(self):
         super(EFF_FEWA, self).startGame()
         self.statistics = np.ones(shape=(3, self.nbArms, 2)) * np.nan
         self.windows = np.array([1, int(np.ceil(self.m))])
-        self.outlogconst = np.sqrt(self.windows * np.sqrt(8 * self.alpha * self.subgaussian ** 2))
+        self.outlogconst = self._append_thresholds(self.windows)
 
 
 
@@ -113,10 +113,11 @@ class FEWA(EFF_FEWA):
         super(FEWA, self).__init__(nbArms, subgaussian=subgaussian, alpha=alpha,delta = delta, m = 1 + 10**(-15))
 
     def __str__(self):
-        if self.plot_delta:
-            return r"FEWA($\alpha={:.3g}$)".format(self.alpha)
-        else:
+        if self.delta != None:
             return r"FEWA($\alpha={:.3g}, \, \delta ={:.3g}$)".format(self.alpha, self.delta)
+        else:
+            return r"FEWA($\alpha={:.3g}$)".format(self.alpha)
+
 
 
 