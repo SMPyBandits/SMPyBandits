d93d28faba885ccddf660230aa5ee1a339797da2
Julien <julien@MacBook-Air.local>
2019-12-17 09:50:39 +0100

    feat(harmonisation): make beautiful, accessible and harmonized plots

--- SMPyBandits/Experiment/Seznec19a_Fig3/main.py
+++ SMPyBandits/Experiment/Seznec19a_Fig3/main.py
@@ -9,8 +9,8 @@ http://proceedings.mlr.press/v89/seznec19a.html
 https://arxiv.org/abs/1811.11043 (updated version)
 """
 
-from SMPyBandits.Arms import RestedRottingGaussian
-from SMPyBandits.Policies import FEWA, EFF_FEWA, wSWA, GreedyOracle
+from SMPyBandits.Arms import UnboundedGaussian as Gaussian
+from SMPyBandits.Policies import FEWA, GreedyOracle, UCB
 from SMPyBandits.Environment.MAB_rotting import repetedRuns
 import numpy as np
 import datetime
@@ -21,26 +21,25 @@ import sys
 date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 ### SET Policies
 policies = [
-  [FEWA, {'alpha': .06, 'delta': 1}],
-  [EFF_FEWA, {'alpha' : 0.06, 'delta':1, 'm':2}],
-  [wSWA, {'alpha' : 0.002}],
-  [wSWA, {'alpha' : 0.02}],
-  [wSWA, {'alpha' : 0.2}],
+    [FEWA, {'alpha': .01, 'delta': 1, 'subgaussian': 1}],
+    [FEWA, {'alpha': .06, 'delta': 1, 'subgaussian': 1}],
+    [FEWA, {'alpha': 0.25, 'delta': 1, 'subgaussian': 1}],
+    [UCB, {}]
 ]
-policy_ind = 2 if len(sys.argv) == 1 else sys.argv[1]
+policy_ind = 0 if len(sys.argv) == 1 else sys.argv[1]
 policy = policies[policy_ind]
 policy_name = str(policy[0](nbArms=2, **policy[1]))
-policy_name_nospace = policy_name.replace (' ', '_')
+policy_name_nospace = policy_name.replace(' ', '_')
 
-
-os.makedirs('./data/logging/', exist_ok = True)
-logging.basicConfig(filename=os.path.join('./data/logging', date + '.log'), level=logging.INFO, format='%(asctime)s %(message)s')
+os.makedirs('./data/logging/', exist_ok=True)
+logging.basicConfig(filename=os.path.join('./data/logging', date + '.log'), level=logging.INFO,
+                    format='%(asctime)s %(message)s')
 logging.info("Policy : %s$" % (policy_name))
 
-PARALLEL = False # Set positive int to indicate the number of core, -1 to use all the cores, and False to not parallelize
-REPETITIONS =  1 if len(sys.argv) < 3 else sys.argv[2] # Set the number of repetitions
-HORIZON = 25000 # Horizon T
-sigma = 1 # Gaussian noise std
+PARALLEL = -1  # Set positive int to indicate the number of core, -1 to use all the cores, and False to not parallelize
+REPETITIONS = 1000 if len(sys.argv) < 3 else sys.argv[2]  # Set the number of repetitions
+HORIZON = 5000  # Horizon T
+sigma = 1  # Gaussian noise std
 
 ### SET L/2 in figure 1
 logging.info("CONFIG : CPU %s" % os.cpu_count())
@@ -48,29 +47,35 @@ logging.info("CONFIG : REPETITIONS %s" % REPETITIONS)
 logging.info("CONFIG : HORIZON %s" % HORIZON)
 logging.info("CONFIG : SIGMA %s" % sigma)
 
-### SET K arms
-
-mus = [0] + [0.001 * np.sqrt(10) ** (i) for i in range(9)]
-
-def abruptDecayFunction(mui, muf, breakpoint):
-  return lambda n : mui if n < breakpoint else muf
-
+### GAME 1
+logging.info("EVENT : GAME 1 mu = 0.14")
 arms = [
-  [RestedRottingGaussian, {'decayingFunction': abruptDecayFunction(mu, -mu, 1000), 'sigma': sigma, }] for mu in mus
+    [Gaussian, {'sigma': sigma, "mu": 0}],
+    [Gaussian, {'sigma': sigma, "mu": 0.14}],
 ]
 
 rew, noisy_rew, time, pulls, cumul_pulls = repetedRuns(policy, arms, rep=REPETITIONS, T=HORIZON, parallel=PARALLEL)
-oracle_rew, noisy_oracle_rew, oracle_time, oracle_pull, oracle_cumul_pulls = repetedRuns([GreedyOracle, {}], arms, rep=1, T=HORIZON, oracle=True)
+oracle_rew, noisy_oracle_rew, oracle_time, oracle_pull, oracle_cumul_pulls = repetedRuns(
+    [GreedyOracle, {}], arms, rep=1, T=HORIZON, oracle=True
+)
 regret = oracle_rew - rew
-diffpulls = np.abs(cumul_pulls - oracle_cumul_pulls)
-print(diffpulls)
 logging.info("EVENT : SAVING ... ")
-path_regret = os.path.join('./data/', 'REGRET_' + policy_name_nospace + '_' + date )
-path_diffpull = os.path.join('./data/', 'DIFFPULL_' + policy_name_nospace + '_' + date )
+path_regret = os.path.join('./data/', 'REGRET1_' + policy_name_nospace + '_' + date)
 np.save(path_regret, regret)
-np.save(path_diffpull, diffpulls)
+
+### GAME 2
+logging.info("EVENT : GAME 2 mu = 1")
+arms = [
+    [Gaussian, {'sigma': sigma, "mu": 0}],
+    [Gaussian, {'sigma': sigma, "mu": 1}],
+]
+
+rew, noisy_rew, time, pulls, cumul_pulls = repetedRuns(policy, arms, rep=REPETITIONS, T=HORIZON, parallel=PARALLEL)
+oracle_rew, noisy_oracle_rew, oracle_time, oracle_pull, oracle_cumul_pulls = repetedRuns(
+    [GreedyOracle, {}], arms, rep=1, T=HORIZON, oracle=True
+)
+regret = oracle_rew - rew
+logging.info("EVENT : SAVING ... ")
+path_regret = os.path.join('./data/', 'REGRET2_' + policy_name_nospace + '_' + date)
+np.save(path_regret, regret)
 logging.info("EVENT : END ... ")
-
-
-
-
