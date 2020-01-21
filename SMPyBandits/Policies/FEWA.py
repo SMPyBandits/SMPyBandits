3efc93a0f6f1c8fb54dab74938303052c0dbab71
Julien <julien@MacBook-Air.local>
2020-01-21 14:51:21 +0100

    fix(efficient) : repare efficient mechanism

--- SMPyBandits/Policies/FEWA.py
+++ SMPyBandits/Policies/FEWA.py
@@ -21,6 +21,7 @@ __author__ = "Julien Seznec"
 __version__ = "0.1"
 
 import numpy as np
+
 np.seterr(divide='ignore')  # XXX dangerous in general, controlled here!
 
 try:
@@ -36,38 +37,31 @@ class EFF_FEWA(BasePolicy):
     and [Seznec et al.,  2019b, WIP] (m<=2)
     We use the confidence level :math:`\delta_t = \frac{1}{t^\alpha}`.
      """
-    def __init__(self, nbArms, alpha=0.06, subgaussian=1, m = None, delta = None ):
+
+    def __init__(self, nbArms, alpha=0.06, subgaussian=1, m=2, delta=None):
         super(EFF_FEWA, self).__init__(nbArms)
         self.alpha = alpha
         self.nbArms = nbArms
         self.subgaussian = subgaussian
         self.statistics = np.ones(shape=(3, self.nbArms, 2)) * np.nan
-        self.grid = m if m is not None else 2
         # [0,:,:] : current statistics, [1,:,:]: pending statistics, [2,:,:]: number of sample in the pending statistics
-        self.windows = np.array([1, int(np.ceil(self.grid))])
+        self.windows = np.array([1, int(np.ceil(m))])
         self.outlogconst = self._append_thresholds(self.windows)
-        self.delta = delta
-        self.inlogconst = 1/delta**(1/alpha) if delta!= None else 1
+        self.delta = delta if delta != None else 1
+        self.inlogconst = 1 / delta ** (1 / alpha) if delta != None else 1
         self.armSet = np.arange(nbArms)
-        self.display_m = m is not None
+        self.grid = m
+        self.tmp =[]
 
     def __str__(self):
-        if self.delta is not None:
-            if self.display_m:
-                return r"EFF_FEWA($\alpha={:.3g}, \, \delta={:.3g}, \, m={:.3g}$)".format(self.alpha, self.delta, self.grid)
-            else: 
-                return r"EFF_FEWA($\alpha={:.3g}, \, \delta={:.3g}$)".format(self.alpha, self.delta)
+        if self.delta != None:
+            return r"EFF_FEWA($\alpha={:.3g}, \, \delta={:.3g}, \, m={:.3g}$)".format(self.alpha, self.delta, self.grid)
         else:
-            if self.display_m:
-                return r"EFF_FEWA($\alpha={:.3g}, \, m={:.3g}$)".format(self.alpha, self.grid)
-            else: 
-                return r"EFF_FEWA($\alpha={:.3g}$)".format(self.alpha)
-            
-
+            return r"EFF_FEWA($\alpha={:.3g}, \, m={:.3g}$)".format(self.alpha, self.grid)
 
     def getReward(self, arm, reward):
         super(EFF_FEWA, self).getReward(arm, reward)
-        if not np.all(np.isnan(self.statistics[0,:,-1])):
+        if not np.all(np.isnan(self.statistics[1, :, -1])):
             self.statistics = np.append(self.statistics, np.nan * np.ones([3, self.nbArms, 1]), axis=2)
         while self.statistics.shape[2] > min(len(self.outlogconst), len(self.windows)):
             self.windows = np.append(self.windows, int(np.ceil(self.windows[-1] * self.grid)))
@@ -78,16 +72,18 @@ class EFF_FEWA(BasePolicy):
         self.statistics[2, arm, 1:] += 1
         idx = np.where((self.statistics[2, arm, :] == self.windows))[0]
         self.statistics[0, arm, idx] = self.statistics[1, arm, idx]
+        self.tmp.append(np.nanmin(self.statistics[2, arm, :] / self.windows))
         idx_nan = np.where(np.isnan(self.statistics[2, arm, :]))[0]
         idx = np.concatenate([idx, np.array([i for i in idx_nan if i - 1 in set(list(idx))]).astype(int)])
         self.statistics[1:, arm, idx[idx != 0]] = self.statistics[1:, arm, idx[idx != 0] - 1]
+
 
     def choice(self):
         remainingArms = self.armSet.copy()
         i = 0
         selected = remainingArms[np.isnan(self.statistics[0, :, i])]
         sqrtlogt = np.sqrt(np.log(self._inlog()))
-        while len(selected) == 0 :
+        while len(selected) == 0:
             thresh = np.max(self.statistics[0, remainingArms, i]) - sqrtlogt * self.outlogconst[i]
             remainingArms = remainingArms[self.statistics[0, remainingArms, i] >= thresh]
             i += 1
@@ -99,15 +95,13 @@ class EFF_FEWA(BasePolicy):
         return np.sqrt(8 * w * self.alpha * self.subgaussian ** 2)
 
     def _inlog(self):
-        return self.inlogconst * self.t
+        return max(self.inlogconst * self.t, 1)
 
     def startGame(self):
         super(EFF_FEWA, self).startGame()
         self.statistics = np.ones(shape=(3, self.nbArms, 2)) * np.nan
         self.windows = np.array([1, int(np.ceil(self.m))])
         self.outlogconst = self._append_thresholds(self.windows)
-
-
 
 
 class FEWA(EFF_FEWA):
@@ -117,8 +111,9 @@ class FEWA(EFF_FEWA):
     This implementation is valid for $:math:`T < 10^{15}`.
     For :math:`T>10^{15}`, FEWA will have time and memory issues as its time and space complexity is O(KT) per round.
     """
-    def __init__(self, nbArms, subgaussian=1, alpha = 4, delta = None):
-        super(FEWA, self).__init__(nbArms, subgaussian=subgaussian, alpha=alpha,delta = delta, m = 1 + 10**(-15))
+
+    def __init__(self, nbArms, subgaussian=1, alpha=4, delta=None):
+        super(FEWA, self).__init__(nbArms, subgaussian=subgaussian, alpha=alpha, delta=delta, m=1 + 10 ** (-15))
 
     def __str__(self):
         if self.delta != None:
@@ -127,20 +122,13 @@ class FEWA(EFF_FEWA):
             return r"FEWA($\alpha={:.3g}$)".format(self.alpha)
 
 
-
-
-
 if __name__ == "__main__":
     # Code for debugging purposes.
-    HORIZON = 20000
+    HORIZON = 100000
     sigma = 1
-    policy = EFF_FEWA(5, subgaussian=sigma, alpha=0.06, m =1.1)
-    reward = {0: 0, 1: 0.2, 2: 0.4, 3: 0.6, 4: 0.8}
+    policy = EFF_FEWA(1, subgaussian=sigma, alpha=0.06, m=1.1)
+    reward = {0: 2}#, 1: 0.2, 2: 0.4, 3: 0.6, 4: 0.8}
     for t in range(HORIZON):
         choice = policy.choice()
         policy.getReward(choice, reward[choice])
-    print(policy.statistics[0,:,:])
-    print(policy.statistics.shape)
-    print(policy.windows)
-    print(len(policy.windows))
-    print(policy.pulls)
+    print(np.min(policy.tmp))
