3871b64f86a5b75ac2bf25fea7cf4e41f3cf8391
Julien <julien.seznec@lelivrescolaire.fr>
2020-07-28 15:17:24 +0200

    stupid copy paste mistake

--- SMPyBandits/Experiment/Seznec_asymptotic/main.py
+++ SMPyBandits/Experiment/Seznec_asymptotic/main.py
@@ -77,5 +77,5 @@ for m, mu in enumerate(mus):
  # time_res.append(time)
 logging.info("EVENT : SAVING ... ")
 np.save(regret_path, np.array(regret_res))
-#np.save(time_path, np.array(regret_res))
+#np.save(time_path, np.array(time_res))
 logging.info("EVENT : END ... ")
